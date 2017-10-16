1fbe40e2557b35183fc255ca5f821c7451dd67d4
Zhou <hazhou@w14471.davidson.edu>
2017-10-15 22:39:51 -0400

    Quadratic probe and double hash completed

---
+++ hashTables/hashTables/quadraticprobe.h
@@ -0,0 +1,14 @@
+//
+//  quadraticprobe.hpp
+//  hashTables
+//
+//  Created by Zhou, Harry on 10/15/17.
+//  Copyright © 2017 Roy Bhattacharya, Anubhav. All rights reserved.
+//
+
+#ifndef quadraticprobe_hpp
+#define quadraticprobe_hpp
+
+#include <stdio.h>
+
+#endif /* quadraticprobe_hpp */
